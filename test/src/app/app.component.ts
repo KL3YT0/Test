fd9a574b7289c9238f4c558a84fabcb37f4d1944
Ivan Lyukshin <vunu.2012@yandex.ru>
2022-10-04 02:22:45 +0300

    (feature) install tailwind, material ui

---
+++ test/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
