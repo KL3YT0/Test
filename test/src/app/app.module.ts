265244b8a3ffa25f3649468cbe38204fe895b5e8
Ivan Lyukshin <vunu.2012@yandex.ru>
2022-10-05 15:59:34 +0300

    (feature) add datetime-picker package

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,9 +9,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import {
+  NgxMatDatetimePickerModule,
+  NgxMatNativeDateModule,
+  NgxMatTimepickerModule,
+} from '@angular-material-components/datetime-picker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,9 +34,12 @@ import { AddCardComponent } from './components/add-card/add-card.component';
     MatButtonToggleModule,
     MatDatepickerModule,
     MatNativeDateModule,
-
     BrowserAnimationsModule,
     ReactiveFormsModule,
+
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
+    NgxMatTimepickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
